e26e010cdfee41f6076107833646145ad1ff47e9
Ewa <ewa.markowska3@gmail.com>
2023-02-07 04:48:11 +0100

    User profile

--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -1,4 +1,3 @@
-
 import './profile.scss';
 import { useParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
@@ -7,7 +6,7 @@ import React, { useState, useEffect } from 'react';
 function Profile(props) {
   const { Id } = useParams();
   const [user, setUser] = useState({});
- 
+  const [offerDtos, setOfferDtos] = useState([]);
   // https://localhost:7026/api/seekers/${SeekerId}
 
   useEffect(() => {
@@ -16,35 +15,57 @@ function Profile(props) {
       .then(data => setUser(data));
   }, [Id]);
 
+
+  useEffect(() => {
+    fetch(`https://localhost:7052/api/users/${Id}`)
+      .then(response => response.json())
+      .then(data => {
+        setOfferDtos(data.offerDtos);
+      });
+  }, [Id]);
+
   return (
       
       <div className='profile'>
         <div className='images'></div>
-        <img src={require('./img10.jpg',)} className='cover'></img>
+        <img src={require('./img10.jpg')} className='cover' alt='' />
         <img src={require('./user1.png')} alt='' className='profilePic'/>
       <div className='profilecontainer'>
         <div className='userInfo'>
             <div className='leftSide'>
-                <a><i class="fa-solid fa-phone"></i></a>
-                <a><i class="fa-solid fa-envelope"></i></a>
-                <div class="vl"></div>
+                <a><i className="fa-solid fa-phone"></i></a>
+                <a><i className="fa-solid fa-envelope"></i></a>
+                <div className="vl"></div>
                
                 
                 </div>
             <div className='centerSide'>{user.name}</div>
-            <div class="vl"></div>
+            <div className="vl"></div>
             <div className='rightSide'>
-    </div>
-            
-            
-            
-            
+
+
+
+
+
+
+              
+            </div>
+
         </div>
-        <div className='user-details'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis consequuntur qui minima distinctio quo, accusantium error, inventore delectus dignissimos eius eaque ut aut eum ipsa quidem voluptatibus dolorem sequi. Natus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis consequuntur qui minima distinctio quo, accusantium error, inventore delectus dignissimos eius eaque ut aut eum ipsa quidem voluptatibus dolorem sequi. Natus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis consequuntur qui minima distinctio quo, accusantium error, inventore delectus dignissimos eius eaque ut aut eum ipsa quidem voluptatibus dolorem sequi. Natus!
-        
+        <div>
       
-        
+      {offerDtos.map(offerDto => (
+        <div className="info-container" key={offerDto.id}>
+          <p>Typ oferty: {offerDto.name}</p>
+          <p>Opis oferty: {offerDto.description}</p>
+          <p>Cena usługi: {offerDto.priceOffer}</p>
+          {offerDto.address && <p>Adres: {offerDto.address}</p>}
+
+
+
         </div>
+      ))}
+    </div>
         
         
       </div>
